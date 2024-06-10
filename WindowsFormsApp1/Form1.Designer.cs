fb929e3ed5e12adedf86ab9c5db46dc3c3a6b870
Maria <159527845+RaidenMoor@users.noreply.github.com>
2024-06-10 17:24:23 +0400

    Create Form1.Designer.cs

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorMessage = new System.Windows.Forms.Label();
+            this.analise = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.stringOfUserInput = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.constsGrid = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idsGrid = new System.Windows.Forms.ListBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(5, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1360, 537);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1135, 458);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Инофрмация о проекте";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1026, 390);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Gainsboro;
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.errorMessage);
+            this.tabPage2.Controls.Add(this.analise);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.userInput);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1135, 458);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Ввод и анализ строки";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(786, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 50);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Обнулить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorMessage
+            // 
+            this.errorMessage.AutoSize = true;
+            this.errorMessage.Font = new System.Drawing.Font("Microsoft JhengHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.errorMessage.ForeColor = System.Drawing.Color.IndianRed;
+            this.errorMessage.Location = new System.Drawing.Point(28, 243);
+            this.errorMessage.Name = "errorMessage";
+            this.errorMessage.Size = new System.Drawing.Size(0, 23);
+            this.errorMessage.TabIndex = 4;
+            // 
+            // analise
+            // 
+            this.analise.BackColor = System.Drawing.Color.DimGray;
+            this.analise.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.analise.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.analise.Location = new System.Drawing.Point(22, 174);
+            this.analise.Name = "analise";
+            this.analise.Size = new System.Drawing.Size(211, 47);
+            this.analise.TabIndex = 3;
+            this.analise.Text = "АНАЛИЗ";
+            this.analise.UseVisualStyleBackColor = false;
+            this.analise.Click += new System.EventHandler(this.analise_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(633, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите строку, принадлежащую указанной грамматике";
+            // 
+            // userInput
+            // 
+            this.userInput.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.userInput.Location = new System.Drawing.Point(22, 69);
+            this.userInput.Multiline = true;
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(1092, 89);
+            this.userInput.TabIndex = 1;
+            this.userInput.TextChanged += new System.EventHandler(this.userInput_TextChanged);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.stringOfUserInput);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.constsGrid);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.idsGrid);
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1352, 508);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Вывод семантики";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // stringOfUserInput
+            // 
+            this.stringOfUserInput.AutoSize = true;
+            this.stringOfUserInput.Font = new System.Drawing.Font("Montserrat SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stringOfUserInput.Location = new System.Drawing.Point(17, 409);
+            this.stringOfUserInput.Name = "stringOfUserInput";
+            this.stringOfUserInput.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.stringOfUserInput.Size = new System.Drawing.Size(20, 27);
+            this.stringOfUserInput.TabIndex = 7;
+            this.stringOfUserInput.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(17, 365);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(200, 27);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Исходная строка:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // constsGrid
+            // 
+            this.constsGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.constsGrid.FormattingEnabled = true;
+            this.constsGrid.ItemHeight = 29;
+            this.constsGrid.Location = new System.Drawing.Point(281, 30);
+            this.constsGrid.Name = "constsGrid";
+            this.constsGrid.Size = new System.Drawing.Size(221, 323);
+            this.constsGrid.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Montserrat SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(276, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 27);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Константы";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 27);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Идентификаторы";
+            // 
+            // idsGrid
+            // 
+            this.idsGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.idsGrid.FormattingEnabled = true;
+            this.idsGrid.ItemHeight = 29;
+            this.idsGrid.Location = new System.Drawing.Point(8, 30);
+            this.idsGrid.Name = "idsGrid";
+            this.idsGrid.Size = new System.Drawing.Size(221, 323);
+            this.idsGrid.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1364, 561);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Анализатор А-языка";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label errorMessage;
+        private System.Windows.Forms.Button analise;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox idsGrid;
+        private System.Windows.Forms.ListBox constsGrid;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label stringOfUserInput;
+    }
+}
+
